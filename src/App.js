fde4a156da3d52300113ae45b56959645876779a
Anis khemiri <aniskhemiri6@gmail.com>
2021-10-22 19:00:13 +0200

    create Project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Knowledges from './pages/Knowledges';
+import Portfolio from './pages/Portfolio';
+import NotFound from './pages/NotFound';
+
+
+
+
+const App = () => {
+  return (
+    <>
+     <BrowserRouter>
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/knowledges" component={Knowledges} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/contact" component={Contact} />
+      <Route component={NotFound} />
+      </Switch>
+     </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/knowledges/Experience.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+
+const Experience = () => {
+    return (
+        <div className="experience">
+            <h3>Expérience</h3>
+            <div className="exp-1">
+                <h4>Direceur Technique -Apple</h4>
+                <h5>2018-2020</h5>
+                <p>Quick tip: keeping your Arc profile experience and interests updated helps us recommend the best job opportunities for you.
+                    Still not seeing the perfect job opportunity? Let us know your feedback, and help us 
+                    improve the accuracy of your personalized recommendations</p>
+            </div>
+            <div className="exp-2">
+                <h4>Direceur Technique -Apple</h4>
+                <h5>2018-2020</h5>
+                <p>Quick tip: keeping your Arc profile experience and interests updated helps us recommend the best job opportunities for you.
+                    Still not seeing the perfect job opportunity? Let us know your feedback, and help us 
+                    improve the accuracy of your personalized recommendations</p>
+            </div>
+        </div>
+    );
+}
+
+export default Experience;
